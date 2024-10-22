cfe5e55c24a9c1e94020dc034e67a13d4dbb456f
Shaun Agostinho <shaunagostinho@gmail.com>
2024-10-21 23:16:00 -0700

    update fix good?

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -73,6 +74,38 @@ func main() {
 	// Capture 'name' and 'handler' to avoid closure issues
 	app.Post(inboundPath, func(c *fiber.Ctx) error {
 		return carriers["twilio"].Inbound(c, gateway)
+	})
+
+	// Define the GET /media/:id route
+	app.Get("/media/:id", func(c *fiber.Ctx) error {
+		fileID := c.Params("id")
+		if fileID == "" {
+			return fiber.NewError(fiber.StatusBadRequest, "File ID is required")
+		}
+
+		// Retrieve the media file from MongoDB
+		mediaFile, err := getMediaFromMongoDB(gateway.MongoClient, fileID)
+		if err != nil {
+			// Return 404 if the file is not found or any other retrieval error occurs
+			return fiber.NewError(fiber.StatusNotFound, err.Error())
+		}
+
+		// Decode the Base64-encoded data
+		fileBytes, err := base64.StdEncoding.DecodeString(mediaFile.Base64Data)
+		if err != nil {
+			// Return 500 if decoding fails
+			return fiber.NewError(fiber.StatusInternalServerError, "Failed to decode file data")
+		}
+
+		// Set the appropriate Content-Type header
+		c.Set("Content-Type", mediaFile.ContentType)
+
+		// Optionally, set Content-Disposition to suggest a filename for download
+		// Uncomment the following line if you want the browser to prompt a download
+		// c.Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", mediaFile.FileName))
+
+		// Send the file bytes as the response
+		return c.Send(fileBytes)
 	})
 
 	// Start server

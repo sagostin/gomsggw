c9add972cf6e5befe03f759bc864a862b5afdf40
Shaun Agostinho <shaunagostinho@gmail.com>
2025-02-05 18:21:23 +0000

    add more log entries?

--- carrier_telnyx.go
+++ carrier_telnyx.go
@@ -340,6 +340,7 @@ func (h *TelnyxHandler) SendSMS(sms *MsgQueueItem) error {
 				"response_code":   resp.StatusCode,
 				"response_status": resp.Status,
 				"response_body":   string(bodyBytes), // Include response body for debugging
+				"msg":             sms,
 			}, err,
 		))
 		return errors.New("failed to send SMS via Telnyx")
@@ -491,6 +492,7 @@ func (h *TelnyxHandler) SendMMS(mms *MsgQueueItem) error {
 				"response_code":   resp.StatusCode,
 				"response_status": resp.Status,
 				"response_body":   string(bodyBytes), // Include response body for debugging
+				"msg":             mms,
 			}, nil,
 		))
 		return errors.New("failed to send MMS via Telnyx")

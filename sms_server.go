49f634c3bcb2a6431014edd7e30e24fef5eabdbb
Shaun Agostinho <shaunagostinho@gmail.com>
2025-03-17 21:07:15 +0000

    add waiting

--- sms_server.go
+++ sms_server.go
@@ -584,7 +584,7 @@ func (s *SMPPServer) sendSMPP(msg MsgQueueItem, session *smpp.Session) error {
 		if err != nil {
 			return fmt.Errorf("error sending SubmitSM: %v", err)
 		}
-		// time.Sleep(500 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 	}
 	return nil
 }
